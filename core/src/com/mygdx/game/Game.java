722fa4ea55e64c292b807e3f84544cd4ed7aecbb
pedram26 <pedram_764@yahoo.com>
2015-02-05 16:04:51 -0800

    changes added

--- core/src/com/mygdx/game/Game.java
+++ core/src/com/mygdx/game/Game.java
@@ -2,26 +2,72 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+
 
 public class Game extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	Rectangle rect;
+	Vector2 speed;
+	Sprite sprite;
+	boolean fullscreen;
+
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("GodOfWar.jpg");
+		fullscreen = false;
+		//Gdx.graphics.setDisplayMode(1280, 720, fullscreen);
+		rect = new Rectangle(100, 100, 100, 200);
+		speed = new Vector2(10, 10);
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		if (rect.getY() + rect.getHeight() < Gdx.graphics.getHeight()) {
+			if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+				rect.setY(rect.getY() + speed.y);
+				//speed.y++;
+			}
+		}
+
+		if (rect.getY() > 0) {
+			if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+				rect.setY(rect.getY() - speed.y);
+				//speed.y--;
+			}
+		}
+
+		if (rect.getX() + rect.getWidth() < Gdx.graphics.getWidth()){
+			if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+				rect.setX(rect.getX() + speed.x);
+				//speed.x++;
+			}
+		}
+
+		if(rect.getX() > 0) {
+			if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+				rect.setX(rect.getX() - speed.x);
+				//speed.x--;
+			}
+		}
+
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(img, rect.getX(), rect.getY(), rect.getWidth(), rect.getHeight());
 		batch.end();
 	}
 }
