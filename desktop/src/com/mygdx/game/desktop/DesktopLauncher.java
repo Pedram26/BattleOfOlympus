45ae6c32fc65fde2b47407ce38c2f466e2a1c1f1
pedram26 <pedram_764@yahoo.com>
2015-02-23 20:16:21 -0800

    Title of game changed

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -7,7 +7,7 @@ import com.mygdx.game.Game;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.title = "Drop";
+		config.title = "Battle Of Olympus";
 		config.width = 800;
 		config.height = 480;
 		new LwjglApplication(new Game(), config);
